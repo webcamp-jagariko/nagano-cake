1176670007d55029f6a88a06e03926b85bcf58dd
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-07-16 14:36:54 +0000

    [Add] 新規登録・ログイン画面作成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,7 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
+  validates :introduction, length: { maximum: 50 }
 end
