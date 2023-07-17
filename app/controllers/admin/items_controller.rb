02ac1bee80197dfc0c7ad0cec778fb60c3ca6914
ui-a <135043176+ui-a@users.noreply.github.com>
2023-07-17 03:03:38 +0000

    [Add]controller一部記述

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,16 @@
 class Admin::ItemsController < ApplicationController
+
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
   end
 
   def show
@@ -10,4 +18,11 @@ class Admin::ItemsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
-  
+
   has_one_attached :image
-  
+
   has_many :cart_item, dependent: :destroy
   has_many :order_detail, dependent: :destroy
   belongs_to :genre
-  
-  
+
+  validates :name, presence:true, uniqueness: true
+  validates :price, presence:true
 end
