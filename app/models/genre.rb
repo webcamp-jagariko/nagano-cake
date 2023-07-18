fb552041912acf2effd58a9f1c69b326f015eb26
ui-a <135043176+ui-a@users.noreply.github.com>
2023-07-18 02:29:03 +0000

    [update]form箇所修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ApplicationRecord
 
-  has_many :item, dependent: :destroy
+  has_many :items, dependent: :destroy
 
   validates :name, presence:true, uniqueness: true
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,8 @@ class Item < ApplicationRecord
 
   has_one_attached :image
 
-  has_many :cart_item, dependent: :destroy
-  has_many :order_detail, dependent: :destroy
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
   belongs_to :genre
 
   validates :name, presence:true
