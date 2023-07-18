55d281c2e6091bb27567ec7d29be443c85488a98
ui-a <135043176+ui-a@users.noreply.github.com>
2023-07-18 01:24:04 +0000

    [update]一部修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -29,6 +29,7 @@ class Admin::ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
+    @genres = Genre.all
     if @item.update(item_params)
     redirect_to admin_item_path(@item.id)
     else
