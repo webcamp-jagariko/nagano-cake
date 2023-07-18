e39db682ddb363c39a37346c75836e17b91a4ded
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-07-18 13:15:48 +0000

    [Update] 表示する商品を最新順に変更

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
-    @items = Item.order('created_at').limit(4)
+    @items = Item.order(created_at: "DESC").limit(4)
   end
 
   def about
