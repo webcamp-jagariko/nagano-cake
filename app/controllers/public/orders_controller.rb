0c4537e6d16364c7742edf4c233cce4bb6369484
EC2 Default User <ec2-user@ip-172-31-26-182.ap-northeast-1.compute.internal>
2023-07-19 10:45:52 +0000

    [Start]confirm修正中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -17,12 +17,38 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    @address = ShippingAddress.find(params[:order][:address_id])
-    @order.post_code = @address.post_code
-    @order.address = @address.address
-    @order.name = @address.name
     @cart_items = current_customer.cart_items
-    @billing_amount = current_customer.cart_items.billing_amount(@cart_items)
+    @total = 0
+
+
+    if params[:order][:address_number] == "0"
+      @order.post_code = current_customer.post_code
+      @order.address = current_customer.address
+      @order.name = current_customer.last_name + " " + current_customer.first_name
+      render 'confirm'
+    elsif params[:order][:address_number] == "1"
+      @shipping_address = ShippingAddress.find(params[:order][:address_id])
+      @order.post_code = @shipping_address.postcode
+      @order.address = @shipping_address.address
+      @order.name = @shipping_address.name
+      render 'confirm'
+    elsif params[:order][:address_number] == "2"
+      @shipping_address = current_customer.shipping_address.new
+      @shipping_address.address = params[:order][:address]
+      @shipping_address.name = params[:order][:name]
+      @shipping_address.post_code = params[:order][:post_code]
+      @shipping_address.customer_id = current_customer.id
+      if @shipping_address.save
+        @order.post_code = @shipping_address.post_code
+        @order.name = @shipping_address.name
+        @order.address = @shipping_address.address
+      else
+       render 'new'
+      end
+    end
+
+
+
   end
 
   def create
