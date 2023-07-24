b9ba8c160836d3bcb3e7137681da1651fab5faa4
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-07-24 02:21:41 +0000

    [Update] route統合

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-
-
   root to: 'public/homes#top'
+
   scope module: :public do
     get 'customers/mypage' => 'customers#show'
     get 'customers/introduction/edit' => 'customers#edit'
@@ -12,33 +10,19 @@ Rails.application.routes.draw do
 
     get 'homes/top' => 'homes#top'
     get 'homes/about' => 'homes#about'
-  end
-  namespace :admin do
-    resources :orders, only: [:show, :update]
-    resources :order_details, only: [:update]
-  end
 
-  namespace :admin do
-    resources :customers, only: %i[index show edit update]
-  end
-
-  namespace :admin do
-    resources :genres, only: [:index, :create, :edit, :update]
-    resources :genre_searches, only: [:show]
-    resources :order_searches, only: [:show]
-  end
-
-  namespace :admin do
-    resources :items, only: [:index, :new, :create, :show, :edit, :update]
-  end
-
-  namespace :admin do
-    get '/' => 'homes#top'
-  end
-  scope module: :public do
     resources :shipping_addresses, only:  [:index, :create, :edit, :update, :destroy]
-  end
-  scope module: :public do
+    resources :cart_items, only: [:index, :create, :update, :destroy]do
+      collection do
+        delete "all_destroy"
+      end
+    end
+
+    get 'items' => 'items#index'
+    get 'items/:id' => 'items#show', as: 'item_show'
+    get 'search' => 'searches#search'
+    resources :genre_searches, only: [:show]
+
     get 'orders' => 'orders#index'
     get 'orders/new' => 'orders#new'
     get 'orders/complete' => 'orders#complete'
@@ -46,21 +30,17 @@ Rails.application.routes.draw do
     # get 'orders/show' => 'orders#show'
     resources :orders, only:  [:create, :show]
   end
-  scope module: :public do
-    resources :cart_items, only: [:index, :create, :update, :destroy]do
-      collection do
-        delete "all_destroy"
-      end
-    end
-  end
 
-  scope module: :public do
-    get 'items' => 'items#index'
-    get 'items/:id' => 'items#show', as: 'item_show'
-    get 'search' => 'searches#search'
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
+    resources :customers, only: %i[index show edit update]
+    resources :genres, only: [:index, :create, :edit, :update]
     resources :genre_searches, only: [:show]
+    resources :order_searches, only: [:show]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
-
 
 # 顧客用
 # URL /customers/sign_in ...
