b5519c69d23cb4484cb81b7a696f152ced140403
YukiNakayama <121099971+Y-NKYM@users.noreply.github.com>
2023-07-25 08:00:17 +0000

    [Update] レイアウト・その他

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -16,7 +16,7 @@ class Admin::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
-      redirect_to admin_customer_path(@customer.id), notice: "You have updated user successfully."
+      redirect_to admin_customer_path(@customer.id), notice: "変更を保存しました"
     else
       render :edit
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_premitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
-    homes_top_path
+    root_path
   end
 
   protected
